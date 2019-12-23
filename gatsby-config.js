3431de20cf29f661c59cb9def0afb2d172a8cdb9
Michael Baron <Michael.Baron@lmc.eu>
2019-12-23 22:08:17 +0100

    Connected Prismic and Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,49 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+	siteMetadata: {
+		title: `Gatsby Default Starter`,
+		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+		author: `@gatsbyjs`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		{
+			resolve: 'gatsby-source-prismic-graphql',
+			options: {
+				repositoryName: 'firsttestlmc', // (REQUIRED, replace with your own)
+				accessToken: 'MC5YZ0VabVJBQUFDTUE4TjVR.77-977-9LGHvv71l77-9Ww5577-9Jhbvv73vv73vv73vv73vv73vv73vv70w77-9Q--_ve-_ve-_vVwRMjbvv73vv70', // (optional API access token)
+				path: '/preview', // (optional preview path. Default: /preview)
+				previews: true, // (optional, activated Previews. Default: false)
+				pages: [ { // (optional, builds pages dynamically)
+					type: 'news',         // TypeName from prismic
+					match: '/news/:uid',  // Pages will be generated under this pattern
+					path: '/news',        // Placeholder page for unpublished documents
+					component: require.resolve( './src/templates/news.js' ),
+				} ],
+			}
+		}
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
 }
