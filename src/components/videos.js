99ebb8fe488fae2c3e54cf4c43b867dbf4f977f9
Michael Baron <Michael.Baron@lmc.eu>
2019-12-24 17:55:01 +0100

    Fixed videos (no `document` during server side rendering). Added Readme

--- src/components/videos.js
+++ src/components/videos.js
@@ -12,13 +12,14 @@ const Videos = ( { items } ) => {
 
 				return (
 					<div key={`video${index}`} className="videos__item">
-						{React.createElement( el.nodeName.toLowerCase(), {
-							width: el.width,
-							height: el.height,
-							src: el.src,
-							frameBorder: 0,
-							allowFullScreen: true
-						}, null )}
+						<iframe
+							width={el.width}
+							height={el.height}
+							src={el.src}
+							frameBorder="0"
+							allowFullScreen={true}
+							title="Video"
+						/>
 						<h4>{item.video_title[0].text}</h4>
 						{RichText.render( item.video_text )}
 					</div>
